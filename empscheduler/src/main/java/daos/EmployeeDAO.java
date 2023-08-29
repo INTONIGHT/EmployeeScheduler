4650d0569dd17013e35e6aeffaf80a527aa0abed
INTONIGHT <tyler.throop@gmail.com>
2023-08-29 15:57:45 -0700

    starting to build out the structure will transition to sql stuff dont curreently have a db so might use either text files for storage for now

--- empscheduler/src/main/java/daos/EmployeeDAO.java
+++ empscheduler/src/main/java/daos/EmployeeDAO.java
@@ -1,6 +1,29 @@
 package daos;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import modules.Employee;
+
 public class EmployeeDAO {
 	//This will handle setting up methods for an employee as well as anything else
-	
+	//CRUD methods
+	public Employee addEmployee(String firstName, String lastName, double rate) {
+		//ideally you would also call a method in a different class to update the SQL for now I am just going to map values
+		HashMap<String, ArrayList<Double>> times = new HashMap<>();
+		return addEmployee(firstName, lastName,rate, times);
+	}
+	public Employee addEmployee(String firstName, String lastName, double rate, HashMap<String, ArrayList<Double>> timesAvailable) {
+		Employee e = new Employee();
+		e.setFirstName(firstName);
+		e.setLastName(lastName);
+		e.setPayRate(rate);
+		e.setDailyHoursAvailable(timesAvailable);
+		return e;
+	}
+	public Employee getEmployeeById(int id) {
+		Employee e = new Employee();
+		//this is where youd run some sql code so build that out
+		return e;
+	}
 }
--- empscheduler/src/main/java/modules/Employee.java
+++ empscheduler/src/main/java/modules/Employee.java
@@ -5,13 +5,9 @@ import java.util.HashMap;
 import java.util.Objects;
 
 public class Employee {
-	 private String employeeType, firstName, lastName;
-	    private double hoursWorked;
-	    private double overtimeHours;
-	    private double payRate;
-	    private double hoursAvailable;
-	    //Trying to figure out how to store the hours per day for each employee
-	    private HashMap<String,ArrayList<Double>> availableTimes;
+	 	private String employeeType, firstName, lastName;
+	 	private int id;
+	 	private double hoursWorked, overtimeHours, payRate,hoursAvailable;
 	    //per worker the goal will be to have the day then an array list of hours available
 	    //the functionality will be 24hours stored ie 1pm would be 13 and then 15 mins as .25
 	    //the arraylist will follow the pattern of first number is start time second number is end time.
@@ -22,8 +18,7 @@ public class Employee {
 	    }
 		public Employee(String employeeType, String firstName, String lastName, double hoursWorked,
 				double overtimeHours, double payRate, double hoursAvailable,
-				HashMap<String, ArrayList<Double>> availableTimes,
-				HashMap<String, ArrayList<Double>> dailyHoursAvailable) {
+				HashMap<String, ArrayList<Double>> dailyHoursAvailable,int id) {
 			super();
 			this.employeeType = employeeType;
 			this.firstName = firstName;
@@ -32,8 +27,8 @@ public class Employee {
 			this.overtimeHours = overtimeHours;
 			this.payRate = payRate;
 			this.hoursAvailable = hoursAvailable;
-			this.availableTimes = availableTimes;
 			this.dailyHoursAvailable = dailyHoursAvailable;
+			this.id = id;
 		}
 		public String getEmployeeType() {
 			return employeeType;
@@ -77,29 +72,29 @@ public class Employee {
 		public void setHoursAvailable(double hoursAvailable) {
 			this.hoursAvailable = hoursAvailable;
 		}
-		public HashMap<String, ArrayList<Double>> getAvailableTimes() {
-			return availableTimes;
-		}
-		public void setAvailableTimes(HashMap<String, ArrayList<Double>> availableTimes) {
-			this.availableTimes = availableTimes;
-		}
+		
 		public HashMap<String, ArrayList<Double>> getDailyHoursAvailable() {
 			return dailyHoursAvailable;
 		}
 		public void setDailyHoursAvailable(HashMap<String, ArrayList<Double>> dailyHoursAvailable) {
 			this.dailyHoursAvailable = dailyHoursAvailable;
 		}
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
 		@Override
 		public String toString() {
 			return "Employee [employeeType=" + employeeType + ", firstName=" + firstName + ", lastName=" + lastName
 					+ ", hoursWorked=" + hoursWorked + ", overtimeHours=" + overtimeHours + ", payRate=" + payRate
-					+ ", hoursAvailable=" + hoursAvailable + ", availableTimes=" + availableTimes
-					+ ", dailyHoursAvailable=" + dailyHoursAvailable + "]";
+					+ ", hoursAvailable=" + hoursAvailable +  ", dailyHoursAvailable=" + dailyHoursAvailable + ", id=" + id + "]";
 		}
 		@Override
 		public int hashCode() {
-			return Objects.hash(availableTimes, dailyHoursAvailable, employeeType, firstName, hoursAvailable,
-					hoursWorked, lastName, overtimeHours, payRate);
+			return Objects.hash(dailyHoursAvailable, employeeType, firstName, hoursAvailable, hoursWorked, id, lastName,
+					overtimeHours, payRate);
 		}
 		@Override
 		public boolean equals(Object obj) {
@@ -110,14 +105,14 @@ public class Employee {
 			if (getClass() != obj.getClass())
 				return false;
 			Employee other = (Employee) obj;
-			return Objects.equals(availableTimes, other.availableTimes)
-					&& Objects.equals(dailyHoursAvailable, other.dailyHoursAvailable)
+			return Objects.equals(dailyHoursAvailable, other.dailyHoursAvailable)
 					&& Objects.equals(employeeType, other.employeeType) && Objects.equals(firstName, other.firstName)
 					&& Double.doubleToLongBits(hoursAvailable) == Double.doubleToLongBits(other.hoursAvailable)
 					&& Double.doubleToLongBits(hoursWorked) == Double.doubleToLongBits(other.hoursWorked)
-					&& Objects.equals(lastName, other.lastName)
+					&& id == other.id && Objects.equals(lastName, other.lastName)
 					&& Double.doubleToLongBits(overtimeHours) == Double.doubleToLongBits(other.overtimeHours)
 					&& Double.doubleToLongBits(payRate) == Double.doubleToLongBits(other.payRate);
 		}
+		
 	    
 }
