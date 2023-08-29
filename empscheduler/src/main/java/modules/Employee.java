ce520773d19f36d0b2e17884e0337b1caaf86b5d
INTONIGHT <tyler.throop@gmail.com>
2023-08-29 12:45:28 -0700

    Generating some methods will start thinking more on the logic just want to get a good base

--- empscheduler/src/main/java/modules/Employee.java
+++ empscheduler/src/main/java/modules/Employee.java
@@ -2,6 +2,7 @@ package modules;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Objects;
 
 public class Employee {
 	 private String employeeType, firstName, lastName;
@@ -16,4 +17,107 @@ public class Employee {
 	    //the arraylist will follow the pattern of first number is start time second number is end time.
 	    //can have any amt of shifts per day not over 24 hrs.
 	    private HashMap<String, ArrayList<Double>> dailyHoursAvailable;
+	    public Employee() {
+	    	super();
+	    }
+		public Employee(String employeeType, String firstName, String lastName, double hoursWorked,
+				double overtimeHours, double payRate, double hoursAvailable,
+				HashMap<String, ArrayList<Double>> availableTimes,
+				HashMap<String, ArrayList<Double>> dailyHoursAvailable) {
+			super();
+			this.employeeType = employeeType;
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.hoursWorked = hoursWorked;
+			this.overtimeHours = overtimeHours;
+			this.payRate = payRate;
+			this.hoursAvailable = hoursAvailable;
+			this.availableTimes = availableTimes;
+			this.dailyHoursAvailable = dailyHoursAvailable;
+		}
+		public String getEmployeeType() {
+			return employeeType;
+		}
+		public void setEmployeeType(String employeeType) {
+			this.employeeType = employeeType;
+		}
+		public String getFirstName() {
+			return firstName;
+		}
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+		public String getLastName() {
+			return lastName;
+		}
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+		public double getHoursWorked() {
+			return hoursWorked;
+		}
+		public void setHoursWorked(double hoursWorked) {
+			this.hoursWorked = hoursWorked;
+		}
+		public double getOvertimeHours() {
+			return overtimeHours;
+		}
+		public void setOvertimeHours(double overtimeHours) {
+			this.overtimeHours = overtimeHours;
+		}
+		public double getPayRate() {
+			return payRate;
+		}
+		public void setPayRate(double payRate) {
+			this.payRate = payRate;
+		}
+		public double getHoursAvailable() {
+			return hoursAvailable;
+		}
+		public void setHoursAvailable(double hoursAvailable) {
+			this.hoursAvailable = hoursAvailable;
+		}
+		public HashMap<String, ArrayList<Double>> getAvailableTimes() {
+			return availableTimes;
+		}
+		public void setAvailableTimes(HashMap<String, ArrayList<Double>> availableTimes) {
+			this.availableTimes = availableTimes;
+		}
+		public HashMap<String, ArrayList<Double>> getDailyHoursAvailable() {
+			return dailyHoursAvailable;
+		}
+		public void setDailyHoursAvailable(HashMap<String, ArrayList<Double>> dailyHoursAvailable) {
+			this.dailyHoursAvailable = dailyHoursAvailable;
+		}
+		@Override
+		public String toString() {
+			return "Employee [employeeType=" + employeeType + ", firstName=" + firstName + ", lastName=" + lastName
+					+ ", hoursWorked=" + hoursWorked + ", overtimeHours=" + overtimeHours + ", payRate=" + payRate
+					+ ", hoursAvailable=" + hoursAvailable + ", availableTimes=" + availableTimes
+					+ ", dailyHoursAvailable=" + dailyHoursAvailable + "]";
+		}
+		@Override
+		public int hashCode() {
+			return Objects.hash(availableTimes, dailyHoursAvailable, employeeType, firstName, hoursAvailable,
+					hoursWorked, lastName, overtimeHours, payRate);
+		}
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Employee other = (Employee) obj;
+			return Objects.equals(availableTimes, other.availableTimes)
+					&& Objects.equals(dailyHoursAvailable, other.dailyHoursAvailable)
+					&& Objects.equals(employeeType, other.employeeType) && Objects.equals(firstName, other.firstName)
+					&& Double.doubleToLongBits(hoursAvailable) == Double.doubleToLongBits(other.hoursAvailable)
+					&& Double.doubleToLongBits(hoursWorked) == Double.doubleToLongBits(other.hoursWorked)
+					&& Objects.equals(lastName, other.lastName)
+					&& Double.doubleToLongBits(overtimeHours) == Double.doubleToLongBits(other.overtimeHours)
+					&& Double.doubleToLongBits(payRate) == Double.doubleToLongBits(other.payRate);
+		}
+	    
 }
--- empscheduler/src/main/java/modules/Store.java
+++ empscheduler/src/main/java/modules/Store.java
@@ -2,6 +2,7 @@ package modules;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Objects;
 
 public class Store {
 	//how many hours total for a day the store needs
@@ -19,5 +20,99 @@ public class Store {
 	//this will have the individual day and then have a map which will map each individual hour of the day
 	//then put number of hours needed.
 	private HashMap<String, HashMap<Double,Double>> dailyTimePerHour;
+	public Store() {
+		super();
+	}
+	public Store(double dailyHoursNeeded, double dailyHoursAvailable, double weeklyHoursNeeded,
+			double weeklyHoursAvailable, double dailyCost, double weeklyCost,
+			HashMap<String, ArrayList<Double>> hoursPerDay, HashMap<String, HashMap<Double, Double>> dailyTimePerHour) {
+		super();
+		this.dailyHoursNeeded = dailyHoursNeeded;
+		this.dailyHoursAvailable = dailyHoursAvailable;
+		this.weeklyHoursNeeded = weeklyHoursNeeded;
+		this.weeklyHoursAvailable = weeklyHoursAvailable;
+		this.dailyCost = dailyCost;
+		this.weeklyCost = weeklyCost;
+		this.hoursPerDay = hoursPerDay;
+		this.dailyTimePerHour = dailyTimePerHour;
+	}
+	public double getDailyHoursNeeded() {
+		return dailyHoursNeeded;
+	}
+	public void setDailyHoursNeeded(double dailyHoursNeeded) {
+		this.dailyHoursNeeded = dailyHoursNeeded;
+	}
+	public double getDailyHoursAvailable() {
+		return dailyHoursAvailable;
+	}
+	public void setDailyHoursAvailable(double dailyHoursAvailable) {
+		this.dailyHoursAvailable = dailyHoursAvailable;
+	}
+	public double getWeeklyHoursNeeded() {
+		return weeklyHoursNeeded;
+	}
+	public void setWeeklyHoursNeeded(double weeklyHoursNeeded) {
+		this.weeklyHoursNeeded = weeklyHoursNeeded;
+	}
+	public double getWeeklyHoursAvailable() {
+		return weeklyHoursAvailable;
+	}
+	public void setWeeklyHoursAvailable(double weeklyHoursAvailable) {
+		this.weeklyHoursAvailable = weeklyHoursAvailable;
+	}
+	public double getDailyCost() {
+		return dailyCost;
+	}
+	public void setDailyCost(double dailyCost) {
+		this.dailyCost = dailyCost;
+	}
+	public double getWeeklyCost() {
+		return weeklyCost;
+	}
+	public void setWeeklyCost(double weeklyCost) {
+		this.weeklyCost = weeklyCost;
+	}
+	public HashMap<String, ArrayList<Double>> getHoursPerDay() {
+		return hoursPerDay;
+	}
+	public void setHoursPerDay(HashMap<String, ArrayList<Double>> hoursPerDay) {
+		this.hoursPerDay = hoursPerDay;
+	}
+	public HashMap<String, HashMap<Double, Double>> getDailyTimePerHour() {
+		return dailyTimePerHour;
+	}
+	public void setDailyTimePerHour(HashMap<String, HashMap<Double, Double>> dailyTimePerHour) {
+		this.dailyTimePerHour = dailyTimePerHour;
+	}
+	@Override
+	public String toString() {
+		return "Store [dailyHoursNeeded=" + dailyHoursNeeded + ", dailyHoursAvailable=" + dailyHoursAvailable
+				+ ", weeklyHoursNeeded=" + weeklyHoursNeeded + ", weeklyHoursAvailable=" + weeklyHoursAvailable
+				+ ", dailyCost=" + dailyCost + ", weeklyCost=" + weeklyCost + ", hoursPerDay=" + hoursPerDay
+				+ ", dailyTimePerHour=" + dailyTimePerHour + "]";
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(dailyCost, dailyHoursAvailable, dailyHoursNeeded, dailyTimePerHour, hoursPerDay, weeklyCost,
+				weeklyHoursAvailable, weeklyHoursNeeded);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Store other = (Store) obj;
+		return Double.doubleToLongBits(dailyCost) == Double.doubleToLongBits(other.dailyCost)
+				&& Double.doubleToLongBits(dailyHoursAvailable) == Double.doubleToLongBits(other.dailyHoursAvailable)
+				&& Double.doubleToLongBits(dailyHoursNeeded) == Double.doubleToLongBits(other.dailyHoursNeeded)
+				&& Objects.equals(dailyTimePerHour, other.dailyTimePerHour)
+				&& Objects.equals(hoursPerDay, other.hoursPerDay)
+				&& Double.doubleToLongBits(weeklyCost) == Double.doubleToLongBits(other.weeklyCost)
+				&& Double.doubleToLongBits(weeklyHoursAvailable) == Double.doubleToLongBits(other.weeklyHoursAvailable)
+				&& Double.doubleToLongBits(weeklyHoursNeeded) == Double.doubleToLongBits(other.weeklyHoursNeeded);
+	}
 	
 }
