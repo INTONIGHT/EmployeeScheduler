366148d49712265e4ea99d0b9f3d80b0e74dea95
INTONIGHT <tyler.throop@gmail.com>
2023-09-22 07:58:22 -0700

    starting to refactor

---
+++ empscheduler/src/main/java/com/employeescheduler/EmployeeSchedulerApplication.java
@@ -0,0 +1,10 @@
+package com.employeescheduler;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSchedulerApplication {
+   public static void main(String[] args) {
+	   SpringApplication.run(EmployeeSchedulerApplication.class,args);
+   }
+}
--- empscheduler/src/main/java/com/employeescheduler/Main.java
+++
@@ -1,7 +0,0 @@
-package com.employeescheduler;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
